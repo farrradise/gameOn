8ea305aea2e04174990db443ca7e8c1a9366787d
Farrah <marzini.f@gmail.com>
2022-06-06 18:49:50 +0200

    Activation du bouton x de fermeture du modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelectorAll(".close"); // FRRH
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -18,6 +19,15 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+
+// Event listener on closeBtn to close modal
+closeBtn[0].addEventListener("click", closeModal); // FRRH
+
+// Close Modal on click x // FRRH
+function closeModal() {
+  modalbg.style.display = "none";
 }
 
 
